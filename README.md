6a714fb3f0272efd73b5ddc978ee30de80c06c9f
muyong88 <muyong88@qq.com>
2020-03-08 11:55:18 +0800

    feature: fix some problem
    1、use EngineGroup().Sync2
    2、udpate structure in  readme
    3、update IgnoreURLs in app.yaml
    null

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # gofront
 
 #  目录结构
+    +-- /public 所有的样式文件(css文件），前端脚本，网站小图标，上传的图片，打包生成的js文件都存放在这个目录
+    | +-- /public/assets/css 
+    | +-- /public/assets/js 
+    | +-- /public/assets/css 
+    | +-- /public/assets/images 
+    | +-- /public/assets/fonts 
     +-- /config 配置目录
     +-- /db 数据库文件目录
-    +-- /src go代码的存放目录
     +-- /log 日志目录
+    +-- /src go代码的存放目录
+    | +-- /src/controller 处理器代码
+    ||+-- /src/controller/websocket_controller  websocket处理器代码
     | +-- /src/controller 处理器代码
     | +-- /src/inits 初始化代码
     | +-- /src/model 数据模型
--- main.go
+++ main.go
@@ -26,10 +26,8 @@ func main() {
 	tmpl.Reload(true)                     // 在每个请求上 重新加载模板（开发模式）
 	app.RegisterView(tmpl)
 
-	gofrontdb.MasterEngine().Sync2(new(model.CCTC_Process_State))     //库结构同步
-	gofrontdb.SlaveEngine().Sync2(new(model.CCTC_Process_State))      //库结构同步
-	gofrontdb.MasterEngine().Sync2(new(model.Protocal_Process_State)) //库结构同步
-	gofrontdb.SlaveEngine().Sync2(new(model.Protocal_Process_State))  //库结构同步
+	gofrontdb.EngineGroup().Sync2(new(model.CCTC_Process_State))     //库结构同步
+	gofrontdb.EngineGroup().Sync2(new(model.Protocal_Process_State)) //库结构同步
 
 	controller.Init_network("config/conf/NetWork.xml") // init net
 	controller.Run_network()                           // Receive network data
