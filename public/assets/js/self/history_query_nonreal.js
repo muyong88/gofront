7a52a4ecd0042e64753f5e5862bcddd249de23d7
muyong88 <muyong88@qq.com>
2020-05-18 18:08:02 +0800

    feature:fix
    1.cctc query's  batch-command-button was put on wrong place
    2.batch command navigate wrong content on ctcc,protocal,nonreal history query pages
    3.batch-command-button should hide in role 2
    
    null

--- public/assets/js/self/history_query_nonreal.js
+++ public/assets/js/self/history_query_nonreal.js
@@ -64,47 +64,64 @@ function updateNonrealTable(data){
     var objs = eval("("+data+")"); 
     var table = jQuery('#nonreal_tb').DataTable();
     table.clear();
-    var curtime= new Date();
+    // var curtime= new Date();
     jQuery.each(objs, function (index, obj) {
-        var updatetime=new Date(obj.UpDateTime);
-        var datediffSecond=Math.round((curtime.getTime()-updatetime.getTime())/1000);
-        if(datediffSecond>300){
-            table.row.add([
-                '<font color="grey">'+obj.UpDateTime+'</font>',
-                '<font color="grey">'+obj.MsgTag+'</font>',
-                '<font color="grey">'+obj.MsgType+'</font>',
-                '<font color="grey">'+obj.MissionID+'</font>',
-                '<font color="grey">'+obj.Subtype+'</font>',
-                '<font color="grey">'+obj.MSGID+'</font>',
-                '<font color="grey">'+obj.Sender+'</font>',
-                '<font color="grey">'+obj.Timestamp+'</font>',
-                '<font color="grey">'+obj.Type+'</font>',
-                '<font color="grey">'+obj.SendSessionID+'</font>',
-                '<font color="grey">'+obj.FileName+'</font>',
-                '<font color="grey">'+obj.FilePath+'</font>',
-                '<font color="grey">'+obj.Status+'</font>',
-                '<font color="grey">'+obj.Station+'</font>',
+             table.row.add([
+                obj.UpDateTime,
+                obj.MsgTag,
+                obj.MsgType,
+                obj.MissionID,
+                obj.Subtype,
+                obj.MSGID,
+                obj.Sender,
+                obj.Timestamp,
+                obj.Type,
+                obj.SendSessionID,
+                obj.FileName,
+                obj.FilePath,
+                obj.Status,
+                obj.Station,
                 '<a href="/non_real/commandpage?MsgTag='+obj.MsgTag+'&MsgType='+obj.MsgType+'&MissionID='+obj.MissionID+'&Subtype='+obj.Subtype+'&MSGID='+obj.MSGID+'&Sender='+obj.Sender+'&SendSessionID='+obj.SendSessionID+'" target="_blank" style="color:red;">发送命令</a>'
             ]); 
-        }else{
-            table.row.add([
-                '<font color="black">'+obj.UpDateTime+'</font>',
-                '<font color="black">'+obj.MsgTag+'</font>',
-                '<font color="black">'+obj.MsgType+'</font>',
-                '<font color="black">'+obj.MissionID+'</font>',
-                '<font color="black">'+obj.Subtype+'</font>',
-                '<font color="black">'+obj.MSGID+'</font>',
-                '<font color="black">'+obj.Sender+'</font>',
-                '<font color="black">'+obj.Timestamp+'</font>',
-                '<font color="black">'+obj.Type+'</font>',
-                '<font color="black">'+obj.SendSessionID+'</font>',
-                '<font color="black">'+obj.FileName+'</font>',
-                '<font color="black">'+obj.FilePath+'</font>',
-                '<font color="black">'+obj.Status+'</font>',
-                '<font color="black">'+obj.Station+'</font>',
-                '<a href="/non_real/commandpage?MsgTag='+obj.MsgTag+'&MsgType='+obj.MsgType+'&MissionID='+obj.MissionID+'&Subtype='+obj.Subtype+'&MSGID='+obj.MSGID+'&Sender='+obj.Sender+'&SendSessionID='+obj.SendSessionID+'" target="_blank" style="color:red;">发送命令</a>'
-            ]); 
-        }
+        // var updatetime=new Date(obj.UpDateTime);
+        // var datediffSecond=Math.round((curtime.getTime()-updatetime.getTime())/1000);
+        // if(datediffSecond>300){
+        //     table.row.add([
+        //         '<font color="grey">'+obj.UpDateTime+'</font>',
+        //         '<font color="grey">'+obj.MsgTag+'</font>',
+        //         '<font color="grey">'+obj.MsgType+'</font>',
+        //         '<font color="grey">'+obj.MissionID+'</font>',
+        //         '<font color="grey">'+obj.Subtype+'</font>',
+        //         '<font color="grey">'+obj.MSGID+'</font>',
+        //         '<font color="grey">'+obj.Sender+'</font>',
+        //         '<font color="grey">'+obj.Timestamp+'</font>',
+        //         '<font color="grey">'+obj.Type+'</font>',
+        //         '<font color="grey">'+obj.SendSessionID+'</font>',
+        //         '<font color="grey">'+obj.FileName+'</font>',
+        //         '<font color="grey">'+obj.FilePath+'</font>',
+        //         '<font color="grey">'+obj.Status+'</font>',
+        //         '<font color="grey">'+obj.Station+'</font>',
+        //         '<a href="/non_real/commandpage?MsgTag='+obj.MsgTag+'&MsgType='+obj.MsgType+'&MissionID='+obj.MissionID+'&Subtype='+obj.Subtype+'&MSGID='+obj.MSGID+'&Sender='+obj.Sender+'&SendSessionID='+obj.SendSessionID+'" target="_blank" style="color:red;">发送命令</a>'
+        //     ]); 
+        // }else{
+        //     table.row.add([
+        //         '<font color="black">'+obj.UpDateTime+'</font>',
+        //         '<font color="black">'+obj.MsgTag+'</font>',
+        //         '<font color="black">'+obj.MsgType+'</font>',
+        //         '<font color="black">'+obj.MissionID+'</font>',
+        //         '<font color="black">'+obj.Subtype+'</font>',
+        //         '<font color="black">'+obj.MSGID+'</font>',
+        //         '<font color="black">'+obj.Sender+'</font>',
+        //         '<font color="black">'+obj.Timestamp+'</font>',
+        //         '<font color="black">'+obj.Type+'</font>',
+        //         '<font color="black">'+obj.SendSessionID+'</font>',
+        //         '<font color="black">'+obj.FileName+'</font>',
+        //         '<font color="black">'+obj.FilePath+'</font>',
+        //         '<font color="black">'+obj.Status+'</font>',
+        //         '<font color="black">'+obj.Station+'</font>',
+        //         '<a href="/non_real/commandpage?MsgTag='+obj.MsgTag+'&MsgType='+obj.MsgType+'&MissionID='+obj.MissionID+'&Subtype='+obj.Subtype+'&MSGID='+obj.MSGID+'&Sender='+obj.Sender+'&SendSessionID='+obj.SendSessionID+'" target="_blank" style="color:red;">发送命令</a>'
+        //     ]); 
+        // }
 });
      
     var currentPage = table.page();
